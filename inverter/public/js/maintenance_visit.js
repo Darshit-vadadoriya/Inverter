f3af0af52d6232d0c4bde0793841f81f6c5d6258
Darshit-vadadoriya <darshpatelvadadoriya@gmail.com>
2024-11-17 21:05:27 +0530

    resolve path issue in api

--- inverter/public/js/maintenance_visit.js
+++ inverter/public/js/maintenance_visit.js
@@ -10,7 +10,7 @@ frappe.ui.form.on('Maintenance Visit', {
 function set_serialno_filter(frm){
      if (frm.doc.customer) {
             frappe.call({
-                method: 'inverter.api.maintenance_visit.get_serial_no',
+                method: 'inverter.apis.maintenance_visit.get_serial_no',
                 args: {
                     customer: frm.doc.customer
                 },
